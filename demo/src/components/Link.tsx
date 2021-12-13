54d9cfdb731c8cf63577a42b9f84122ffe4688e7
Blade Barringer <blade@crookedneighbor.com>
2021-12-13 09:58:24 -0600

    chore: fix linting errors

--- demo/src/components/Link.tsx
+++ demo/src/components/Link.tsx
@@ -1,14 +1,6 @@
-import {
-  NavLink as ReactRouterLink,
-  LinkProps,
-  useMatch,
-  useResolvedPath,
-} from "react-router-dom";
+import { NavLink as ReactRouterLink, LinkProps } from "react-router-dom";
 
 export function Link({ children, to, ...props }: LinkProps) {
-  let resolved = useResolvedPath(to);
-  let match = useMatch({ path: resolved.pathname, end: true });
-
   return (
     <>
       <ReactRouterLink to={to} {...props}>
